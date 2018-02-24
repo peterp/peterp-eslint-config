0190c554abcf95bb66983c2b1e5e4fc851362ba1
Peter Pistorius <peter.pistorius@gmail.com>
2018-02-24 08:05:47 +0100

    Add no-use-before-define rule.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,35 @@
 module.exports = {
-  parser: "babel-eslint",
-  extends: ["eslint-config-airbnb"],
+  parser: 'babel-eslint',
+  extends: ['eslint-config-airbnb'],
   env: {
-    jest: true
+    jest: true,
+  },
+  globals: {
+    fetch: true,
   },
   rules: {
-    "new-cap": 0,
-    "arrow-body-style": 0,
-    "comma-dangle": ["error", "always-multiline"],
-    "arrow-parens": ["error", "as-needed"],
+    'new-cap': 0,
+    'arrow-body-style': 0,
+    'comma-dangle': ['error', 'always-multiline'],
+    'arrow-parens': ['error', 'as-needed'],
 
-    "import/no-extraneous-dependencies:": 0,
-    "import/no-unresolved": 0,
-    "import/extensions": 0,
-    "import/prefer-default-export": 0,
+    'import/no-extraneous-dependencies:': 0,
+    'import/no-unresolved': 0,
+    'import/extensions': 0,
+    'import/prefer-default-export': 0,
+    'no-use-before-define': 0,
 
-    "react/jsx-filename-extension": 0,
-    "react/prefer-stateless-function": 0,
-    "react/forbid-prop-types": 0,
-    "react/no-unescaped-entities": 0,
-    "react/require-default-props": 0,
-    "react/sort-comp": 0,
-    "react/prop-types": [
-      "error",
+    'react/jsx-filename-extension': 0,
+    'react/prefer-stateless-function': 0,
+    'react/forbid-prop-types': 0,
+    'react/no-unescaped-entities': 0,
+    'react/require-default-props': 0,
+    'react/sort-comp': 0,
+    'react/prop-types': [
+      'error',
       {
-        ignore: ["style", "children"]
-      }
-    ]
-  }
+        ignore: ['style', 'children'],
+      },
+    ],
+  },
 };
